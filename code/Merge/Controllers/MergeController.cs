b9d539ddcef3c0775413349061876eb676e30eb6
Andra <andra.vasilcoiu@yahoo.com>
2021-06-10 14:15:44 +0100

    Merge Service Controller

--- code/Merge/Controllers/MergeController.cs
+++ code/Merge/Controllers/MergeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -13,5 +14,20 @@ namespace Merge.Controllers
     {
         // personURL: https://localhost:44345/
         // answerURL: https://localhost:44398/ 
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            // call the Person service
+            var personService = "https://localhost:44345/persons";
+            var personResponseCall = await new HttpClient().GetStringAsync(personService);
+
+            // call the Answer service
+            var answerService = "https://localhost:44398/answers";
+            var answerResponseCall = await new HttpClient().GetStringAsync(answerService);
+
+            var mergeResponse = $"{personResponseCall}{answerResponseCall}";
+            return Ok(mergeResponse);
+        }
     }
 }
