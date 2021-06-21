ba6bbd5c36e586e8865a35b8cdd10970779861e7
Andra <andra.vasilcoiu@yahoo.com>
2021-06-21 10:47:21 +0100

    Added pictures for readme

--- code/FrontEnd/Controllers/HomeController.cs
+++ code/FrontEnd/Controllers/HomeController.cs
@@ -51,10 +51,10 @@ namespace FrontEnd.Controllers
             repository.Save();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
     }
 }
